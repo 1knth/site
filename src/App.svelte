6390622571d133005a97525231ace39118539b2f
Kenneth <knthyang@gmail.com>
2026-06-05 22:34:19 -0400

    init

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  import Counter from './lib/Counter.svelte'
+	import Me from './assets/me.jpeg'
+</script>
+
+<section id="center">
+    <div id="main">
+      <div id="card">
+        <h1>kenneth</h1>
+				<img id="pfp" src={Me} >
+        <div id="desc">
+					<p> inquisitive explorer </p>
+					<div id="links">
+							<a href="https://open.spotify.com/user/31q3fbmkgoh52x3k7ku2tvrrmqwq" target="_blank"> Spotify </a>
+							<a href="https://github.com/1knth" target="_blank"> Github </a>
+					</div>
+        </div>
+      </div>
+    </div>
+</section>
+
+
+<section id="spacer"></section>
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,9 @@
+<script>
+  let count = $state(0)
+</script>
+
+<button type="button" class="counter"
+        onclick={() => count++}
+>
+    Nelson is: {count}
+</button>
